d7c96fcb79f298a2de39d82312e4fd6509ac5b68
zdx0823 <15916965182@163.com>
2021-04-04 01:18:36 +0800

    局部视图

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,4 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js');
 
-mix.sass('resources/css/app.scss', 'public/css');
+mix.sass('resources/css/app.scss', 'public/css').version();
