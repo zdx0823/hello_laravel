27687f0318370ac11650147547c724eda4a251d1
zdx0823 <15916965182@163.com>
2021-04-04 10:22:45 +0800

    注册页面

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+Route::get('/sigup', 'UsersController@create')->name('sigup');
