588127d7109993643ebcaf5622f3d3d272c9cb32
zdx0823 <15916965182@163.com>
2021-04-04 16:00:46 +0800

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
-Route::get('/sigup', 'UsersController@create')->name('sigup');
+Route::get('/sigup', 'UsersController@create')->name('signup');
 
 Route::resource('users', 'UsersController');
+
+Route::get('/login', 'SessionController@create')->name('login');
+Route::post('/login', 'SessionController@store')->name('login');
+Route::delete('/logout', 'SessionController@destroy')->name('logout');
