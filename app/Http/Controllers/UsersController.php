91fde35929d6b121c3b89195a403945a96e8d95f
zdx0823 <15916965182@163.com>
2021-04-04 17:52:49 +0800

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,18 @@ use App\Models\User;
 class UsersController extends Controller
 {
 
+  public function __construct () {
+
+    // 中间件，鉴权，处理show,create,store页面
+    $this->middleware('auth', [
+      'except' => ['show', 'create', 'store']
+    ]);
+
+    $this->middleware('guest', [
+      'only' => ['create']
+    ]);
+  }
+
   public function create () {
     return view('users.create');
   }
@@ -36,11 +48,14 @@ class UsersController extends Controller
 
 
   public function edit (User $user) {
+    $this->authorize('update', $user);
+
     return view('users.edit', compact('user'));
   }
 
 
   public function update (User $user, Request $request) {
+    $this->authorize('update', $user);
     $this->validate($request, [
       'name' => 'required|max:50',
       'password' => 'nullable|confirmed|min:6'
