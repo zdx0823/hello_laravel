1cfdd9e719b67fb9a7f6f5feda204839198002ad
zdx0823 <15916965182@163.com>
2021-04-06 00:14:24 +0800

    限流，throttle:10,60

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,6 +20,10 @@ class UsersController extends Controller
     $this->middleware('guest', [
       'only' => ['create']
     ]);
+
+    $this->middleware('throttle:10,60', [
+      'only' => ['store']
+    ]);
   }
 
   public function index () {
