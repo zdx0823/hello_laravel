0b900b5748fb881e3ac580daaaa9d323ec47da91
zdx0823 <15916965182@163.com>
2021-04-06 10:28:36 +0800

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use \App\Models\Status;
 
 class StatusesController extends Controller
 {
@@ -21,6 +22,13 @@ class StatusesController extends Controller
         ]);
 
         session()->flash('success', '发布成功');
+        return redirect()->back();
+    }
+
+    public function destroy (Status $status) {
+        $this->authorize('destroy', $status);
+        $status->delete();
+        session()->flash('success', '微博已被成功删除');
         return redirect()->back();
     }
 }
