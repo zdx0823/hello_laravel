fbffc790aec9fe1f72556c4c076a6b8fc3b205bb
zdx0823 <15916965182@163.com>
2021-04-06 12:09:43 +0800

    首页加入了别人的文章，修改了User模型的feed方法

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,7 +66,9 @@ class User extends Authenticatable
     }
    
     public function feed () {
-        return $this->statuses()->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)->with('user')->orderBy('created_at', 'desc');
     }
 
     // 粉丝列表：假设当前用户是 id = 1；  user_id = 1，的follower_id 列表就是粉丝列表
